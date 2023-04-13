86a5689f3e2a0addbac90e8ef6366a44f3a59702
andufr12 <andufr12@naver.com>
2023-04-13 09:52:12 +0900

    js01-11.js 페이지 제목 추가

--- js01/js/js01-11.js
+++ js01/js/js01-11.js
@@ -1,6 +1,8 @@
 let list = ``
 let num = prompt("!(값을 입력하지 마세요)");
 
+list += `<h1 style="color:white;">증감연산자 문제</h1>`
+list += `<hr/>`
 list += `<table class="a">`;
 list += `<tr><th>증감연산자</th><th>설명</th><th>예)let num = ${num}</th>  <th>순서</th></tr>`;
 list += `<tr><td>변수++</td><td>기존의 변수 값에 1을 더합니다(후위).</td><td>console.log(num++) 답:${num++} </td><td>1</td></tr>`;
