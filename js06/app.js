5a4159b3aa3df8f0af6367b90b32d5ed15ae44f5
andufr12 <andufr12@naver.com>
2023-04-20 17:46:44 +0900

    랜덤번호 배열에 넣기 까지

---
+++ js06/app.js
@@ -0,0 +1,22 @@
+// const dice = ['1','2','3','4','5','6']
+//         const per = [15,15,25,15,15,15]
+//         let roll = ''
+//         const ran = Math.floor((Math.random)*99)+1
+
+//         for(let i=0;i<1000000;i++){
+//             for(let j=0;j<dice.length;j++){
+//                 if(per[j]>=ran){
+//                 roll = dice[j]
+//                 } else if(per[j]<ran){
+//                     roll = dice[j]
+//                 }
+//             }  
+//         }
+
+
+
+
+const arr =[11,11,22,33]
+
+
+console.log([...new Set(arr)])
